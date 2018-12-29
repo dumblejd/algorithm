fad2d36ebdcc6475a515a63f6ab4c4dcabad30a5
Di Jin <332548128@qq.com>
2018-12-29 11:54:27 -0600

    96tree

--- Tree.java
+++ Tree.java
@@ -1,6 +1,19 @@
 package algorithmtest;
 
 public class Tree {
+	// #96
+	public int numTrees(int n) {
+		int[] num = new int[n + 1];
+		num[0]=1;
+		for(int i=1;i<n+1;i++)
+		{
+			for(int j=0;j<i;j++)
+			{
+				num[i]+=num[j]*num[i-j-1];
+			}
+		}
+		return num[n];
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
