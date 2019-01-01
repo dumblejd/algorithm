6a361b46950f49e8ef17941c9769d5874e694a24
Di Jin <332548128@qq.com>
2019-01-01 17:42:53 -0600

    104tree

--- Tree.java
+++ Tree.java
@@ -229,7 +229,14 @@ public class Tree {
 		travel(curr.left, sol, level + 1);
 		travel(curr.right, sol, level + 1);
 	}
-
+	//#104
+public int maxDepth(TreeNode root) {
+        if(root==null)
+        {
+        	return 0;
+        }
+        	return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TreeNode a = new TreeNode(5);
