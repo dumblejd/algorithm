bb8595b61034eafe870c05e12733072467dd8355
Di Jin <332548128@qq.com>
2019-01-25 15:54:13 -0600

    10 backtrack

--- BackTrack.java
+++ BackTrack.java
@@ -12,7 +12,7 @@ public boolean isMatch(String s, String p) {
         return helper10(s, p);
     }
 
-	public boolean helper10(String s, String p)   //failed version
+	public boolean helper10(String s, String p)   //complicated version
 	{
 		boolean flag=false;
 		if(s.length()==0&&p.length()==0)
@@ -58,7 +58,52 @@ public boolean isMatch(String s, String p) {
 		}
 		return flag;
 	}
-	
+	public boolean helper10_tryToSimplify(String s, String p)   //complicated version
+	{
+		boolean flag=false;
+		if(s.length()==0&&p.length()==0)
+		{
+			return true;
+		}
+		if(s.length()==0)
+		{
+			if(p.length()>=2&&p.charAt(1)=='*')
+			{
+				return helper10( s,p.substring(2));
+			}
+			return false;
+		}
+		if(p.length()==0)
+		{
+			return false;
+		}
+		if(2<=p.length()&&p.charAt(1)=='*')
+		{
+			if(p.charAt(0)==s.charAt(0)||p.charAt(0)=='.')
+			{
+				flag=flag||helper10(s, p.substring(2, p.length()));
+				flag=flag||helper10(s.substring(1), p);
+			}
+			else
+			{
+				flag=flag||helper10(s, p.substring(2, p.length()));
+			}
+		}
+		
+		else
+		{
+			if(p.charAt(0)=='.'||p.charAt(0)==s.charAt(0))
+			{
+				flag=flag||helper10(s.substring(1), p.substring(1));
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+		return flag;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BackTrack bt = new BackTrack();
