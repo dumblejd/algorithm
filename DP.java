535d79fce9440043358008b76a38efe6eb487317
Di Jin <332548128@qq.com>
2019-02-26 12:04:57 -0600

    dp 416

--- DP.java
+++ DP.java
@@ -250,6 +250,45 @@ public class DP {
 		}
 		return false;
 	}
+	//416. Partition Equal Subset Sum
+	 public boolean canPartition(int[] nums) {
+		 int sum=0;
+	        for(int i:nums) {
+	        	sum+=i;
+	        }
+	        if(sum%2==1)
+	        {
+	        	return false;
+	        }
+	     int half=sum/2;
+	     boolean [][]dp=new boolean[nums.length+1][sum+1];
+	     dp[0][0]=true;
+	     // 第一列
+	        for (int i = 1;i<=nums.length; i++) {
+	            dp[i][0] = true;
+	        }
+	        
+	        // 第一行
+	        for (int j = 1; j<=sum; j++) {
+	            dp[0][j] = false;
+	        }
+	        
+	     for(int i=1;i<=nums.length;i++)
+	     {
+	    	 for(int j=1;j<=sum;j++)
+	    	 {
+	    		if(nums[i-1]<=j)
+	    		{
+	    			dp[i][j]=dp[i-1][j]||dp[i-1][j-nums[i-1]];
+	    		}
+	    		else
+	    		{
+	    			dp[i][j] = dp[i - 1][j];
+	    		}
+	    	 }
+	     }
+	     return dp[nums.length][half];
+	    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Map<String,Integer> m = new HashMap<String,Integer>();
